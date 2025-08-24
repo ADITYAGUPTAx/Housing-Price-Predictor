70fe531b6afa38c84b869a065059063876b5f3dc
Aditya Gupta <adityagupta041050@gmail.com>
2025-08-24 16:54:03 +0530

    change in server

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: gunicorn server.server:app
 
 
+
--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-import util
+from . import util   # relative import
 
 app = Flask(__name__)
 
