311cf548eb86d13d83442dafc2cddd3bff0de1ce
Aditya gupta <114644420+ADITYAGUPTAx@users.noreply.github.com>
2025-08-26 14:05:12 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,47 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-
-import json
-import pickle
-import numpy as np
-import pandas as pd
-
-__locations = None
-__data_columns = None
-__model = None
-
-def get_price(location, sqft, bhk, bath):
-   
-    try:
-        loc_index = __data_columns.index(location.lower())
-    except ValueError:
-        loc_index = -1
-
-    # Create input array with correct dtype
-    x = np.zeros(len(__data_columns))
-    x[0] = bhk
-    x[1] = sqft
-    x[2] = bath
-    if loc_index >= 0:
-        x[loc_index] = 1
-
-    # Do NOT force dtype=int, keep as float
-    x_df = pd.DataFrame([x], columns=__data_columns, dtype=float)
-    return round(__model.predict(x_df)[0], 2)
-
-
-def getlocations():
-    loadartifacts()
-    return __locations
-
-def loadartifacts():
-    global __locations, __data_columns, __model
-    print("Loading artifacts......")
-    with open("C:\\Users\\aditya\\Documents\\Bangalore housing price predictor\\server\\artifacts\\columns.json", 'r') as f:
-        __data_columns = json.load(f)["data_columns"]
-        __locations = __data_columns[3:]
-    with open("C:\\Users\\aditya\\Documents\\Bangalore housing price predictor\\server\\artifacts\\model.pickle", 'rb') as f:
-        __model = pickle.load(f)
-    print("Artifacts loaded successfully")
+from . import util
 
 app = Flask(__name__)
 
@@ -52,7 +10,7 @@ def home():
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
-        'locations':getlocations()
+        'locations':util.getlocations()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
@@ -66,11 +24,11 @@ def predict_home_price():
     bath = int(data['bath'])
 
     response = jsonify({
-        'estimated_price':get_price(location, sqft, bhk, bath)
+        'estimated_price':util.get_price(location, sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == "__main__":
-   loadartifacts()
-   app.run(debug=True)
+    util.loadartifacts()
+    app.run(debug=True)
